6457a28ef43e135edce0a4db5e34247ce32efb04
Gideon Chrapko <gideonchrapko@hotmail.ca>
2021-04-09 18:48:49 -0700

    :

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import GayestShitEver from './img/untitled7.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={GayestShitEver} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
